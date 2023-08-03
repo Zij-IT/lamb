c57571bae26799186db7c7f4f3e1433f987a9eb1
Elijah Hartvigsen <elijah.reed@hartvigsen.xyz>
2023-08-03 20:49:43 +0200

    main: Fix bug in compiling case expressions

--- lamb-rs/lamb_ffi/src/c/compile/ast.cpp
+++ lamb-rs/lamb_ffi/src/c/compile/ast.cpp
@@ -524,10 +524,11 @@ CompileAstResult compile(Vm &vm, Compiler *compiler, AstNode *node) {
             AstNode *branch = node->kids[1];
             AstNode *next_arm = node->kids[2];
 
+            i32 offset_before_arm = STACK_DIFF(compiler, 0);
             // Compare the value of the arm with a duplicate of the case value
             compiler->write_op(vm, OpDup);
             BUBBLE(compile(vm, compiler, value));
-            compiler->chunk().write(vm, OpEq);
+            compiler->write_op(vm, OpEq);
 
             i32 if_neq = compiler->chunk().write_jump(vm, OpJumpIfFalse);
 
@@ -536,6 +537,7 @@ CompileAstResult compile(Vm &vm, Compiler *compiler, AstNode *node) {
             compiler->write_op(vm, OpPop);
 
             // Pop the case value off of the stack
+            // This sets the offset to `-1` from the perspective of the arm
             compiler->write_op(vm, OpPop);
 
             // Run through the body of the arm
@@ -550,23 +552,26 @@ CompileAstResult compile(Vm &vm, Compiler *compiler, AstNode *node) {
             // Pop the 'false' from the previous EQ check off of the stack
             compiler->write_op(vm, OpPop);
 
+            // When attempting another arm, the offset for this arm should
+            // be the same as the offset for the arm before, as testing an
+            // arm shouldn't effect the stack difference.
+            compiler->block->offset = offset_before_arm;
+
             if (next_arm != nullptr) {
                 // Attempt the next arm
                 BUBBLE(compile(vm, compiler, next_arm));
-                STACK_DIFF(compiler, -1);
             } else {
                 // Pop the compare value off of the stack
                 compiler->write_op(vm, OpPop);
 
                 // We can't check for exhaustivity at compile time, so we write
                 // a default nil in the event none of arms matched successfully
-                compiler->chunk().write_const(vm, Value::nil());
+                compiler->write_const(vm, Value::nil());
             }
             // If not equal <-------
 
             // If successfull, we can jump over all of the arms thanks to the
             // recursive nature of the case arms representation
-            STACK_DIFF(compiler, +1);
             compiler->chunk().patch_jump(past_else);
             break;
         }
