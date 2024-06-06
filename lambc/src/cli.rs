07bd8b96d096fa3d187e12650484b92d419ebbcf
Zij-IT <elijah.reed@hartvigsen.xyz>
2024-06-06 20:52:08 +0200

    cleanup: remove dead code in lambc

--- lambc/src/cli.rs
+++ lambc/src/cli.rs
@@ -1,45 +1,10 @@
 use std::path::PathBuf;
 
-use clap::{Parser, ValueEnum};
-
-#[repr(C)]
-#[derive(ValueEnum, Clone, Copy, Debug, PartialEq)]
-pub enum OptLevel {
-    None,
-    Basic,
-    Some,
-    All,
-}
-
-#[repr(C)]
-#[derive(ValueEnum, Clone, Copy, Debug, PartialEq)]
-pub enum DebugLevel {
-    None,
-    Basic,
-    Full,
-}
-
-#[repr(C)]
-#[derive(ValueEnum, Clone, Copy, Debug, PartialEq)]
-pub enum GcDebugLevel {
-    None,
-    Alloc,
-    Collection,
-    AllocAndCollection,
-}
+use clap::Parser;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 pub struct LambOptions {
-    #[arg(short, long, value_enum, default_value_t = DebugLevel::None)]
-    pub debug_level: DebugLevel,
-
-    #[arg(short, long, value_enum, default_value_t = GcDebugLevel::None)]
-    pub gc_debug_level: GcDebugLevel,
-
-    #[arg(short, long, value_enum, default_value_t = OptLevel::None)]
-    pub optimization_level: OptLevel,
-
     pub path: PathBuf,
 }
 
--- lambc/src/main.rs
+++ lambc/src/main.rs
@@ -6,12 +6,7 @@ mod cli;
 
 fn main() -> Result<()> {
     human_panic::setup_panic!();
-    let cli::LambOptions {
-        debug_level: _,
-        gc_debug_level: _,
-        optimization_level: _,
-        path,
-    } = cli::LambOptions::parse();
 
+    let cli::LambOptions { path } = cli::LambOptions::parse();
     Ok(lambc_vm::run_script(path)?)
 }
